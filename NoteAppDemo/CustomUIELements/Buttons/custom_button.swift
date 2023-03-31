9c54590192023910bdd6aa8a15fa322fe04e5a9a
Ravi <ravi.l@simformsolutions.com>
2023-03-31 15:54:11 +0530

    note app demo solved using getter setter method type expression issue

--- NoteAppDemo/CustomUIELements/Buttons/custom_button.swift
+++ NoteAppDemo/CustomUIELements/Buttons/custom_button.swift
@@ -41,28 +41,113 @@ struct CustomButton: View {
 
 struct CustomButtonStyle: ButtonStyle {
     
-    let width, height, contentPadding, cornerRadius: CGFloat?
-    let alignment: Alignment?
-    let backgroundColor, foregroundColor, borderColor: Color?
-    let borderWidth: Double?
-    let lineLimit: Int?
+    private var _width: CGFloat = UIScreen.main.bounds.size.width
+    private var _height: CGFloat = 50
+    private var _contentPadding: CGFloat = 0
+    private var _cornerRadius: CGFloat = 0
+    private var _alignment: Alignment = Alignment.center
+    private var _borderWidth: Double = 1.0
+    private var _lineLimit: Int = 1
+    private var _backgroundColor: Color = Color.white
+    private var _foregroundColor: Color = Color.black
+    private var _borderColor: Color = Color.black
     
-    init(){}
+    var width: CGFloat {
+        get {
+            return _width;
+        } set {
+            self._width = newValue
+        }
+    }
+    
+    var height: CGFloat {
+        get {
+            return _height;
+        } set {
+            self._height = newValue
+        }
+    }
+    
+    var contentPadding: CGFloat {
+        get {
+            return _contentPadding;
+        } set {
+            self._contentPadding = newValue
+        }
+    }
+    
+    var cornerRadius: CGFloat {
+        get {
+            return _cornerRadius;
+        } set {
+            self._cornerRadius = newValue
+        }
+    }
+    
+    var alignment: Alignment {
+        get {
+            return _alignment;
+        } set {
+            self._alignment = newValue
+        }
+    }
+    
+    var backgroundColor: Color {
+        get {
+            return _backgroundColor
+        } set {
+            self._backgroundColor = newValue
+        }
+    }
+    
+    var foregroundColor: Color {
+        get {
+            return _foregroundColor
+        } set {
+            self._foregroundColor = newValue
+        }
+    }
+    
+    var borderColor: Color {
+        get {
+            return _borderColor
+        } set {
+            self._borderColor = newValue
+        }
+    }
+    
+    var borderWidth: Double {
+        get {
+            return _borderWidth
+        } set {
+            self._borderWidth = newValue
+        }
+    }
+    
+    var lineLimit: Int {
+        get {
+            return _lineLimit
+        } set {
+            self._lineLimit = newValue
+        }
+    }
+    
+  
     
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .frame(width: width ?? UIScreen.main.bounds.size.width / 1.5, height: height ?? UIScreen.main.bounds.size.height / 30, alignment: alignment ?? Alignment.center)
-            .padding(contentPadding ?? 8)
-            .lineLimit((lineLimit ?? 1))
+            .frame(width: width, height: height, alignment: alignment)
+            .padding(contentPadding)
+            .lineLimit(lineLimit)
             .minimumScaleFactor(0.7)
-            .background(backgroundColor ?? Color.blue)
-            .foregroundColor(foregroundColor ?? Color.white)
+            .background(backgroundColor)
+            .foregroundColor(foregroundColor)
             .scaleEffect(configuration.isPressed ? 1.2 : 1)
             .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
-            .cornerRadius(cornerRadius ?? 10.0)
+            .cornerRadius(cornerRadius)
             .overlay(
-                RoundedRectangle(cornerRadius: cornerRadius ?? 0)
-                    .stroke(borderColor ?? Color.yellow, lineWidth: borderWidth ?? 0)
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .stroke(borderColor, lineWidth: borderWidth)
             )
     }
 }
